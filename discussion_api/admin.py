5ec9bbbbf39bd43a1f2f759808878337a7fcf58e
davidyfxu <82054510@qq.com>
2024-08-03 00:31:03 +0800

    feat: read_api and discussion_api admin

--- discussion_api/admin.py
+++ discussion_api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from discussion_api.models import DiscussionData
+
+
 # Register your models here.
+class DiscussionDataAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'topic_id', 'entry_id', 'entry_user_id', 'entry_workflow_state', 'entry_created_at')
+    search_fields = ('course_id', 'topic_id', 'entry_id', 'entry_user_id')
+    list_filter = ('entry_workflow_state',)
+
+
+admin.site.register(DiscussionData, DiscussionDataAdmin)
--- read_api/admin.py
+++ read_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from read_api.models import ReadData
+
+
+class ReadDataAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'course_code', 'course_workflow_state', 'topic_id', 'topic_title', 'topic_user_id',
+                    'topic_workflow_state', 'entry_id', 'entry_user_id', 'entry_workflow_state', 'entry_read_user_id',
+                    'entry_read_workflow_state', 'entry_created_at')
+    search_fields = ('course_id', 'topic_id', 'entry_id', 'entry_user_id')
+    list_filter = ('course_workflow_state', 'entry_workflow_state', 'topic_workflow_state')
+
+
+admin.site.register(ReadData, ReadDataAdmin)
