f91dc10896128f2c08274deb8f655e1d7912fed7
davidyfxu <82054510@qq.com>
2024-08-06 00:12:37 +0800

    feat: test

--- discussion_api/views.py
+++ discussion_api/views.py
@@ -7,7 +7,10 @@ from discussion_api.serializers import DiscussionSerializer
 
 @api_view(['POST'])
 def get_all_discussion_data(request):
-    course_id = request.POST.get('course_id')
+    print("get_all_discussion_data", request)
+    print("get_all_discussion_data POST", request.POST)
+    print("get_all_discussion_data data", request.body, request.data)
+    course_id = request.POST.get('course_id') or request.data.get('course_id')
     discussions = DiscussionData.objects.get(course_id=course_id).first()
 
     # discussions = DiscussionData.objects.all()
