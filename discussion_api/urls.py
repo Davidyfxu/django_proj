28b0ca09b2ea628d60f1cf4d9c0b6a6f682043e5
davidyfxu <82054510@qq.com>
2024-08-05 23:53:19 +0800

    feat: fix discussion

--- discussion_api/urls.py
+++ discussion_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_all_discussion_data
 
 urlpatterns = [
-    path('discussion/get_all_discussions', get_all_discussion_data, name='get_all_discussion_data'),
+    path('discussion/get_all_discussion_data', get_all_discussion_data, name='get_all_discussion_data'),
 ]
